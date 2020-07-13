70916298a4a39da54f39a80cb3fcb801fc4fbec7
Carlos Rodríguez <havocbcn@gmail.com>
2020-07-13 14:34:56 +0200

    test: improved initial status test

--- test/UnitTests/Domain/TestShould.cs
+++ test/UnitTests/Domain/TestShould.cs
@@ -15,11 +15,25 @@ namespace UnitTest.LIS.Domain {
             var test = new TestExample1(operationFactory);
 
             test.Should().NotBeNull();
+            test.Result.Should().Be(Result.NotTested);
             operationFactory.Received(0).CreateOperation(Arg.Any<string>());
         }
 
         [Theory, AutoData]
         public void AddAOperationByName(string name) {
+            var operationFactory = Substitute.For<IOperationFactory>();
+            var operation = new OperationExample1();
+            operationFactory.CreateOperation(name).Returns(operation);
+
+            var test = new TestExample1(operationFactory);
+            var operationActual = test.AddOperation(name);
+
+            operationFactory.Received(1).CreateOperation(name);
+            operationActual.Should().Be(operation);
+        }    
+
+        [Theory, AutoData]
+        public void ExecuteOperations(string name) {
             var operationFactory = Substitute.For<IOperationFactory>();
             var operation = new OperationExample1();
             operationFactory.CreateOperation(name).Returns(operation);
