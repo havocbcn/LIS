b7bca00a7ba8ba682cbedd0426ccdfd464c01e11
Carlos Rodríguez <havocbcn@gmail.com>
2020-07-13 20:09:25 +0200

    refactor: some minor improvements

--- src/Domain/Tests/BiochemistryTest.cs
+++ src/Domain/Tests/BiochemistryTest.cs
@@ -2,7 +2,7 @@ using LIS.Service;
 
 namespace LIS.Domain.Tests {
     public abstract class BiochemistryTest : Test {
-        public BiochemistryTest(IOperationFactory operationFactory) : base(operationFactory) {
+        protected BiochemistryTest(IOperationFactory operationFactory) : base(operationFactory) {
 
         }
 
